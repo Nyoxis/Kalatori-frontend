55d47e75d32aaac6686e10cb3f68f04a27e08af3
Nyoxis <62454617+Nyoxis@users.noreply.github.com>
2024-02-28 23:00:59 +0200

    Update make_zip.php

--- prestashop_release/make_zip.php
+++ prestashop_release/make_zip.php
@@ -40,8 +40,8 @@ $s=explode("\n",$s); foreach($s as $l) {
 
 echo "create: [$ZIP]\n";
 if(is_file($ZIP)) unlink($ZIP);
-exec("cd \"".$TO."\"; zip -r \"/../../".$ZIP."\" \"./\"");
-exec("cd ../../");
+exec("cd \"".$TO."\"; zip -r \"~/".$ZIP."\" \"./\"");
+exec("cd ~/");
 chmod($ZIP,0666);
 copy($ZIP,"/tmp/".$ZIP);
 
