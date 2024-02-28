93882d65640dd7cf4c9ab4045643a612eff5731e
Nyoxis <62454617+Nyoxis@users.noreply.github.com>
2024-02-28 21:13:53 +0200

    Update make_zip.php

--- prestashop_release/make_zip.php
+++ prestashop_release/make_zip.php
@@ -6,7 +6,7 @@ $FROM="./ps_dotpayment";
 $s=file_get_contents("./prestashop.txt");
 
 $TO="/tmp/UPUPO";
-$ZIP=$_SERVER['PWD']."/$argv[1]";
+$ZIP=$_SERVER['PWD']."/ps_dotpayment84.zip";
 
 exec("rm -r \"".$TO."\"");
 
